bc2448605f3f841a64855e8812f105e90f5ac01a
Knut Peterson <apknut@gmail.com>
2023-03-26 13:40:21 -0500

    Basic lidar subscriber now works

--- src/rbe594_asars/src/lidar_listener.cpp
+++ src/rbe594_asars/src/lidar_listener.cpp
@@ -1,1 +1,16 @@
 // https://github.com/wonwon0/gazebo_contact_republisher
+
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+void dataCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
+	ROS_INFO("LaserScan: (val,angle)-(%f,%f)", msg->range_min, msg->angle_min);
+}
+
+int main(int argc, char **argv){
+	ros::init(argc, argv, "lidar_listener");
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("scan", 1000, dataCallback);
+	ros::spin();
+	return 0;
+}
