b07bd8a6cafdbe32c738de042768f113829d2ef3
Knut Peterson <apknut@gmail.com>
2023-03-26 22:04:34 -0500

    Full pointcloud conversion works! Fixed a bug with frame conversion and added pcl object to save points

--- src/rbe594_asars/src/lidar_listener.cpp
+++ src/rbe594_asars/src/lidar_listener.cpp
@@ -5,6 +5,8 @@
 #include "tf/transform_broadcaster.h"
 #include "message_filters/subscriber.h"
 #include "laser_geometry/laser_geometry.h"
+#include "pcl/point_cloud.h"
+#include "pcl_conversions/pcl_conversions.h"
 
 class LaserScanToPointCloud {
 
@@ -16,11 +18,12 @@ public:
   message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub_;
   tf::MessageFilter<sensor_msgs::LaserScan> laser_notifier_;
   ros::Publisher scan_pub_;
+  pcl::PCLPointCloud2 full_cloud;
 
   LaserScanToPointCloud(ros::NodeHandle n): 
     n_(n),
     laser_sub_(n_, "/scan", 10),
-    laser_notifier_(laser_sub_, tf_, "laser0_frame", 10)
+    laser_notifier_(laser_sub_, tf_, "world", 10)
   {
     laser_notifier_.registerCallback( boost::bind(&LaserScanToPointCloud::scanCallback, this, _1));
     laser_notifier_.setTolerance(ros::Duration(0.01));
@@ -31,14 +34,17 @@ public:
     sensor_msgs::PointCloud2 cloud;
     
     try {
-        projector_.transformLaserScanToPointCloud("laser0_frame", *scan_in, cloud, tf_);
+        projector_.transformLaserScanToPointCloud("world", *scan_in, cloud, tf_);
     }
     catch (tf::TransformException& e) {
         std::cout << e.what();
         return;
     }
 
-	// TODO: add points to a nontransient point cloud object
+	pcl::PCLPointCloud2 newPoints;
+	pcl_conversions::toPCL(cloud, newPoints);
+	pcl::concatenate(full_cloud, newPoints, full_cloud);
+	pcl_conversions::fromPCL(full_cloud, cloud);
 
     scan_pub_.publish(cloud);
 
