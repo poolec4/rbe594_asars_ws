32a61c62a603e55bc1d1ac7ea173016eec6a2e55
Knut Peterson <apknut@gmail.com>
2023-03-26 21:21:29 -0500

    Working transformation of lidar scan data to point cloud in global frame

--- src/rbe594_asars/src/lidar_listener.cpp
+++ src/rbe594_asars/src/lidar_listener.cpp
@@ -1,16 +1,62 @@
-// https://github.com/wonwon0/gazebo_contact_republisher
+#include "ros/ros.h"
+#include "tf/transform_listener.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "tf/message_filter.h"
+#include "tf/transform_broadcaster.h"
+#include "message_filters/subscriber.h"
+#include "laser_geometry/laser_geometry.h"
 
-#include <ros/ros.h>
-#include <sensor_msgs/LaserScan.h>
+class LaserScanToPointCloud {
 
-void dataCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
-	ROS_INFO("LaserScan: (val,angle)-(%f,%f)", msg->range_min, msg->angle_min);
-}
+public:
 
-int main(int argc, char **argv){
-	ros::init(argc, argv, "lidar_listener");
-	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("scan", 1000, dataCallback);
-	ros::spin();
-	return 0;
+  ros::NodeHandle n_;
+  laser_geometry::LaserProjection projector_;
+  tf::TransformListener tf_;
+  message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub_;
+  tf::MessageFilter<sensor_msgs::LaserScan> laser_notifier_;
+  ros::Publisher scan_pub_;
+
+  LaserScanToPointCloud(ros::NodeHandle n): 
+    n_(n),
+    laser_sub_(n_, "/scan", 10),
+    laser_notifier_(laser_sub_, tf_, "laser0_frame", 10)
+  {
+    laser_notifier_.registerCallback( boost::bind(&LaserScanToPointCloud::scanCallback, this, _1));
+    laser_notifier_.setTolerance(ros::Duration(0.01));
+    scan_pub_ = n_.advertise<sensor_msgs::PointCloud2>("/my_cloud",1);
+  }
+
+  void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan_in){
+    sensor_msgs::PointCloud2 cloud;
+    
+    try {
+        projector_.transformLaserScanToPointCloud("laser0_frame", *scan_in, cloud, tf_);
+    }
+    catch (tf::TransformException& e) {
+        std::cout << e.what();
+        return;
+    }
+
+	// TODO: add points to a nontransient point cloud object
+
+    scan_pub_.publish(cloud);
+
+  }
+
+
+	// TODO: make function to rasterize point cloud, transform to height map, and export
+
+
+};
+
+
+int main(int argc, char** argv){
+  
+  ros::init(argc, argv, "my_scan_to_cloud");
+  ros::NodeHandle n;
+  LaserScanToPointCloud lstopc(n);
+  ros::spin();
+  
+  return 0;
 }
